87ec34ff8bdc3eee3e8bb94f240c046ae50b7729
Ulyanov <superulyanov@gmail.com>
2022-01-23 15:36:57 +0300

    Initial commit

---
+++ GitDemo - SwiftBook/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GitDemo - SwiftBook
+//
+//  Created by Alexandr Ulyanov on 23.01.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
